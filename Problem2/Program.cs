5f4c7acd9184716bbe98ad7b4c700eb59d17f195
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2019-08-09 22:18:44 +0200

    Add project files.

---
+++ Problem2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sum = FibonacciSequence().TakeWhile(x => x <= 4_000_000).Where(x => x % 2 == 0).Sum();
+            Console.WriteLine(sum);
+        }
+
+        static IEnumerable<int> FibonacciSequence()
+        {
+            int prev = 1;
+            int current = 1;
+            int temp;
+            yield return prev;
+            while (true)
+            {
+                temp = current;
+                current += prev;
+                yield return current;
+                prev = temp;
+            }
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ProjectEuler.Problem1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = Enumerable.Range(1, 999).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
