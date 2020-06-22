f812a5c6c44f8ed6c8d88267dc87ad66ff56d2ff
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2020-06-22 19:59:20 +0200

    Solve problems 63 and 76

---
+++ ProjectEuler/Problems/001-100/61-70/Problem063.cs
@@ -0,0 +1,36 @@
+﻿using System.Numerics;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems._001_100._61_70
+{
+    /// <summary>
+    /// https://projecteuler.net/problem=63
+    /// </summary>
+    public class Problem063 : IProblem
+    {
+        public Task<string> CalculateAsync(string[] args)
+        {
+            int expBase = 1;
+            int count = 0;
+            while (expBase < 10) // only take numbers under 10 into account
+            {
+                int exp = 1;
+                while (true)
+                {
+                    var result = BigInteger.Pow(expBase, exp);
+                    var resultDigitsCount = result.ToString().Length;
+                    if (resultDigitsCount == exp)
+                    {
+                        count++;
+                    }
+                    else if (resultDigitsCount < exp)
+                        break;
+                    exp++;
+                }
+                expBase++;
+            }
+
+            return Task.FromResult(count.ToString());
+        }
+    }
+}
---
+++ ProjectEuler/Problems/001-100/71-80/Problem076.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems._001_100._71_80
+{
+    public class Problem076 : IProblem
+    {
+        public Task<string> CalculateAsync(string[] args)
+        {
+            var count = AllPossibleSummationsOf(100);
+            return Task.FromResult(count.ToString());
+        }
+        static long AllPossibleSummationsOf(int n)
+        {
+            var arr = Enumerable.Range(1, n - 1).ToArray();
+            var table = new long[n + 1];
+            table[0] = 1;
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = arr[i]; j <= n; j++)
+                    table[j] += table[j - arr[i]];
+            return table[n];
+        }
+    }
+}
