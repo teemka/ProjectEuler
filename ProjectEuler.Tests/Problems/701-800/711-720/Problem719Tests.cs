c6f09ad49af8999173ed42b82af731a08e1f1ab1
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2022-03-13 23:01:53 +0100

    Solve Problem 719

---
+++ ProjectEuler.Tests/Problems/701-800/711-720/Problem719Tests.cs
@@ -0,0 +1,54 @@
+﻿using ProjectEuler.Problems._701_800._711_720;
+
+namespace ProjectEuler.Tests.Problems._701_800._721_730;
+
+public class Problem719Tests : ProblemTestBase
+{
+    public override IProblem Problem => new Problem719();
+
+    public override string Answer => "128088830547982";
+
+    [Theory]
+    [InlineData(81)]
+    [InlineData(6724)]
+    [InlineData(8281)]
+    [InlineData(9801)]
+    public void Should_Split_ReturnTrue(long number)
+    {
+        // Arrange
+        var sqrt = (long)Math.Sqrt(number);
+
+        // Act
+        var result = Problem719.TrySplit(sqrt, number);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(64)]
+    public void Should_Split_ReturnFalse(long number)
+    {
+        // Arrange
+        var sqrt = (long)Math.Sqrt(number);
+
+        // Act
+        var result = Problem719.TrySplit(sqrt, number);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Should_SolveExample()
+    {
+        // Arrange
+        var args = new[] { "10000" };
+
+        // Act
+        var result = await this.Problem.CalculateAsync(args);
+
+        // Assert
+        result.Should().Be("41333");
+    }
+}
---
+++ ProjectEuler/Problems/701-800/711-720/Problem719.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ProjectEuler.Problems._701_800._711_720;
+
+/// <summary>
+/// https://projecteuler.net/problem=719
+/// </summary>
+public class Problem719 : IProblem
+{
+    public static bool TrySplit(long target, long number)
+    {
+        if (target < 0)
+        {
+            return false;
+        }
+
+        var n = number.ToString().AsSpan();
+
+        if (n.Length == 1)
+        {
+            return false;
+        }
+
+        for (int i = 1; i < n.Length; i++)
+        {
+            var left = n[..i];
+            var right = n[i..];
+
+            var leftN = long.Parse(left);
+            var rightN = long.Parse(right);
+
+            var sum = leftN + rightN;
+            if (sum == target)
+            {
+                return true;
+            }
+
+            if (sum < target)
+            {
+                continue;
+            }
+
+            if (TrySplit(target - leftN, rightN) ||
+                TrySplit(target - rightN, leftN))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Task<string> CalculateAsync(string[] args)
+    {
+        long limit = 1_000_000_000_000; // 10^12
+
+        if (args.Any())
+        {
+            limit = long.Parse(args[0]);
+        }
+
+        var i = 1;
+        var n = 1L;
+        var sum = 0L;
+        while (n <= limit)
+        {
+            n = (long)Math.Pow(i, 2);
+
+            if (n % 9 <= 1 && TrySplit(i, n))
+            {
+                sum += n;
+            }
+
+            i++;
+        }
+
+        return Task.FromResult(sum.ToString());
+    }
+}
