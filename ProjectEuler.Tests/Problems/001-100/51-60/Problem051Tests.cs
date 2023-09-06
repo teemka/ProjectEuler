2d2a3bd644ffe7b2f66d5467270161be782bf90f
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2023-09-06 22:14:16 +0200

    Solve Problem 51

---
+++ ProjectEuler.Tests/Problems/001-100/51-60/Problem051Tests.cs
@@ -0,0 +1,53 @@
+﻿using ProjectEuler.Problems._001_100._51_60;
+
+namespace ProjectEuler.Tests.Problems._001_100._51_60;
+
+public class Problem051Tests : ProblemTestBase
+{
+    public override IProblem Problem => new Problem051();
+
+    public override string Answer => "121313";
+
+    [Theory]
+    [InlineData("6", "13")]
+    [InlineData("7", "56003")]
+    public async Task Should_SolveExamples(string target, string expected)
+    {
+        // Arrange
+        var args = new[] { target };
+
+        // Act
+        var result = await this.Problem.CalculateAsync(args);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Should_GenerateWildcards_2Digits()
+    {
+        // Arrange
+        var prime = "13";
+        var expected = new[] { "*3", "1*", "**" };
+
+        // Act
+        var wildcards = Problem051.MakeWildcards(prime);
+
+        // Assert
+        wildcards.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Should_GenerateWildcards_3Digits()
+    {
+        // Arrange
+        var prime = "123";
+        var expected = new[] { "*23", "**3", "1*3", "*2*", "1**", "12*", "***" };
+
+        // Act
+        var wildcards = Problem051.MakeWildcards(prime);
+
+        // Assert
+        wildcards.Should().BeEquivalentTo(expected);
+    }
+}
