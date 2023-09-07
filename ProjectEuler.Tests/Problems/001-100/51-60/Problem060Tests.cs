923ba9904bca4f7ba2e12d200173fb8a4efa9959
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2023-09-07 14:41:10 +0200

    Solve Problem 60

---
+++ ProjectEuler.Tests/Problems/001-100/51-60/Problem060Tests.cs
@@ -0,0 +1,23 @@
+﻿using ProjectEuler.Problems._001_100._51_60;
+
+namespace ProjectEuler.Tests.Problems._001_100._51_60;
+
+public class Problem060Tests : ProblemTestBase
+{
+    public override IProblem Problem => new Problem060();
+
+    public override string Answer => "26033";
+
+    [Fact]
+    public async Task Should_SolveExample()
+    {
+        // Arrange
+        var args = new[] { "4" };
+
+        // Act
+        var result = await this.Problem.CalculateAsync(args);
+
+        // Assert
+        result.Should().Be("792");
+    }
+}
--- ProjectEuler.Tests/Problems/801-900/801-810/Problem808Tests.cs
+++ ProjectEuler.Tests/Problems/801-900/801-810/Problem808Tests.cs
@@ -1,5 +1,4 @@
-﻿using ProjectEuler.Helpers;
-using ProjectEuler.Problems._801_900._801_810;
+﻿using ProjectEuler.Problems._801_900._801_810;
 
 namespace ProjectEuler.Tests.Problems._801_900._801_810;
 
