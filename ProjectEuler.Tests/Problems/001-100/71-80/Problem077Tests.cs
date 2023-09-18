963a1474e383c826bff5f2e7b666079d32903473
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2023-09-18 20:57:03 +0200

    Solve Problem 77

---
+++ ProjectEuler.Tests/Problems/001-100/71-80/Problem077Tests.cs
@@ -0,0 +1,23 @@
+﻿using ProjectEuler.Problems._001_100._71_80;
+
+namespace ProjectEuler.Tests.Problems._001_100._71_80;
+
+public class Problem077Tests : ProblemTestBase
+{
+    public override IProblem Problem => new Problem077();
+
+    public override string Answer => "71";
+
+    [Fact]
+    public async Task Should_SolveExample()
+    {
+        // Arrange
+        var args = new[] { "4" };
+
+        // Act
+        var result = await this.Problem.CalculateAsync(args);
+
+        // Assert
+        result.Should().Be("10");
+    }
+}
