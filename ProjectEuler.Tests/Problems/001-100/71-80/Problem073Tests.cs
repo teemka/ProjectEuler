4c5268be5f446a209921fd7f46224e7c4e164c7f
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2024-05-25 00:22:33 +0200

    Solve Problem 73

---
+++ ProjectEuler.Tests/Problems/001-100/71-80/Problem073Tests.cs
@@ -0,0 +1,18 @@
+﻿using ProjectEuler.Problems._001_100._71_80;
+
+namespace ProjectEuler.Tests.Problems._001_100._71_80;
+
+public class Problem073Tests : ProblemTestBase
+{
+    public override IProblem Problem => new Problem073();
+
+    public override string Answer => "7295372";
+
+    [Fact]
+    public async Task Should_CalculateExample()
+    {
+        var result = await this.Problem.CalculateAsync(["8"]);
+
+        result.Should().Be("3");
+    }
+}
---
+++ ProjectEuler/Problems/001-100/71-80/Problem073.cs
@@ -0,0 +1,32 @@
+﻿namespace ProjectEuler.Problems._001_100._71_80;
+
+/// <summary>
+/// Counting Fractions in a Range
+/// https://projecteuler.net/problem=73
+/// </summary>
+internal class Problem073 : IProblem
+{
+    public Task<string> CalculateAsync(string[] args)
+    {
+        if (!int.TryParse(args.FirstOrDefault(), out var size))
+        {
+            size = 12_000;
+        }
+
+        var count = 0;
+
+        // Use Farey Sequence
+        for (var denominator = 3; denominator <= size; denominator++)
+        {
+            for (var numerator = denominator / 3 + 1; numerator <= denominator / 2; numerator++)
+            {
+                if (NumberHelper.GCD(numerator, denominator) == 1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return Task.FromResult(count.ToString());
+    }
+}
