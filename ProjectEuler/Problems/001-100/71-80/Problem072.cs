04c6371a3f2599066ab81e5c9acae557c1b35e0e
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2024-08-05 23:28:08 +0200

    Solve Problem72 (#49)

---
+++ ProjectEuler/Problems/001-100/71-80/Problem072.cs
@@ -0,0 +1,50 @@
+﻿using Fractions;
+
+namespace ProjectEuler.Problems._001_100._71_80;
+
+/// <summary>
+/// Counting Fractions
+/// https://projecteuler.net/problem=72
+/// </summary>
+public class Problem072 : IProblem
+{
+    public Task<string> CalculateAsync(string[] args)
+    {
+        if (!int.TryParse(args.FirstOrDefault(), out var size))
+        {
+            size = 1_000_000;
+        }
+
+        // Farey Sequence (without 0/1 and 1/1)
+        var farey = Phi(size) - 1;
+
+        return Task.FromResult(farey.ToString());
+    }
+
+    /// <summary>
+    /// Totient summatory function.
+    /// </summary>
+    public static long Phi(int n)
+    {
+        var sum = 0L;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += EulersTotient(i);
+        }
+
+        return sum;
+    }
+
+    public static int EulersTotient(int n)
+    {
+        // Euler's product formula
+        var factors = n.PrimeFactors().Distinct();
+        var product = new Fraction(n, 1);
+        foreach (var factor in factors)
+        {
+            product *= Fraction.One - new Fraction(1, factor, false);
+        }
+
+        return product.ToInt32();
+    }
+}
