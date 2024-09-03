9994702605f5b326826f75e16c745df18fa6cbbc
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2024-09-03 21:48:06 +0200

    Add script

---
+++ generate-coverage.sh
@@ -0,0 +1,4 @@
+rm -rf Coverage
+rm -rf ProjectEuler.Tests/TestResults
+dotnet test -v normal -c Release --collect:"XPlat Code Coverage"
+reportgenerator -reports:ProjectEuler.Tests/TestResults/**/coverage.cobertura.xml -targetdir:Coverage
