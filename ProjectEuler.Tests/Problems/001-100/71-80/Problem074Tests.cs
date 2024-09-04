e43e670211acb6a800199b9e402ade31781339c0
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2024-09-04 23:47:51 +0200

    Solve Problem 74

---
+++ ProjectEuler.Tests/Problems/001-100/71-80/Problem074Tests.cs
@@ -0,0 +1,35 @@
+﻿using ProjectEuler.Problems._001_100._71_80;
+
+namespace ProjectEuler.Tests.Problems._001_100._71_80;
+
+public class Problem074Tests : ProblemTestBase
+{
+    public override IProblem Problem => new Problem074();
+
+    public override string Answer => "402";
+
+    [Theory]
+    [InlineData(69, 5)]
+    [InlineData(78, 4)]
+    [InlineData(540, 2)]
+    public void Should_CalculateExampleLength(int input, int expected)
+    {
+        // Act
+        var chain = new List<long>();
+        Problem074.FillChain(chain, input);
+
+        // Assert
+        chain.Count.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(145, 145)]
+    [InlineData(169, 363601)]
+    [InlineData(363601, 1454)]
+    [InlineData(1454, 169)]
+    public void Should_CalculateDigitFactorial(int input, int expected)
+    {
+        Problem074.DigitFactorial(input).Should().Be(expected);
+    }
+}
---
+++ ProjectEuler/Problems/001-100/71-80/Problem074.cs
@@ -0,0 +1,65 @@
+﻿namespace ProjectEuler.Problems._001_100._71_80;
+
+/// <summary>
+/// Digit Factorial Chains
+/// https://projecteuler.net/problem=74
+/// </summary>
+internal sealed class Problem074 : IProblem
+{
+    public Task<string> CalculateAsync(string[] args)
+    {
+        if (!int.TryParse(args.FirstOrDefault(), out var limit))
+        {
+            limit = 1_000_000;
+        }
+
+        var count = 0;
+        var chain = new List<long>();
+
+        for (var i = 0L; i < limit; i++)
+        {
+            FillChain(chain, i);
+
+            if (chain.Count == 60)
+            {
+                count++;
+            }
+
+            chain.Clear();
+        }
+
+        return Task.FromResult(count.ToString());
+    }
+
+    public static void FillChain(List<long> chain, long n)
+    {
+        while (true)
+        {
+            chain.Add(n);
+            n = DigitFactorial(n);
+
+            if (chain.Contains(n))
+            {
+                break;
+            }
+        }
+    }
+
+    public static long DigitFactorial(long n) =>
+        n.GetDigits().Sum(Factorial);
+
+    private static long Factorial(long n) => n switch
+    {
+        0 => 1,
+        1 => 1,
+        2 => 2,
+        3 => 6,
+        4 => 24,
+        5 => 120,
+        6 => 720,
+        7 => 5040,
+        8 => 40320,
+        9 => 362880,
+        _ => throw new ArgumentException("Expected a digit"),
+    };
+}
