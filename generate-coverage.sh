1055a7b289b9266956fa2de6954da6139ef5e470
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-02-25 21:28:11 +0100

    Unify scripts

--- generate-coverage.sh
+++ generate-coverage.sh
@@ -2,3 +2,4 @@ rm -rf Coverage
 rm -rf ProjectEuler.Tests/TestResults
 dotnet test -v normal -c Release --collect:"XPlat Code Coverage"
 reportgenerator -reports:ProjectEuler.Tests/TestResults/**/coverage.cobertura.xml -targetdir:Coverage
+start Coverage/index.html
