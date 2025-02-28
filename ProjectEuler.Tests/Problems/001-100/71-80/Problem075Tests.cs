14ed49eba79796a467337311b1b6e0b12e230f43
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-02-28 21:37:02 +0100

    Solve Problem 75 with brute force

---
+++ ProjectEuler.Tests/Problems/001-100/71-80/Problem075Tests.cs
@@ -0,0 +1,20 @@
+﻿using ProjectEuler.Problems._001_100._71_80;
+
+namespace ProjectEuler.Tests.Problems._001_100._71_80;
+
+public class Problem075Tests : ProblemTestBase
+{
+    protected override IProblem Problem => new Problem075();
+
+    protected override string Answer => "161667";
+
+    [Fact]
+    public async Task Should_CalculateExample()
+    {
+        // Act
+        var result = await new Problem075().CalculateAsync(["120"]);
+
+        // Assert
+        Assert.Equal("13", result);
+    }
+}
