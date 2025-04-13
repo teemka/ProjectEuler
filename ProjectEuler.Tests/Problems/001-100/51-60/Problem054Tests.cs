8cabd48dc8166d1d376ef0d373477b48b7964b35
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-04-13 22:44:29 +0200

    Fix spaces

--- ProjectEuler.Tests/Problems/001-100/51-60/Problem054Tests.cs
+++ ProjectEuler.Tests/Problems/001-100/51-60/Problem054Tests.cs
@@ -9,16 +9,16 @@ public class Problem054Tests : ProblemTestBase
     protected override string Answer => "376";
 
     [Theory]
-    [InlineData(new[] {"5H", "5C", "6S", "7S", "KD"}, new[] {"2C", "3S", "8S", "8D", "TD"}, -1)]
-    [InlineData(new[] {"5D", "8C", "9S", "JS", "AC"}, new[] {"2C", "5C", "7D", "8S", "QH"}, 1)]
-    [InlineData(new[] {"2D", "9C", "AS", "AH", "AC"}, new[] {"3D", "6D", "7D", "TD", "QD"}, -1)]
-    [InlineData(new[] {"4D", "6S", "9H", "QH", "QC"}, new[] {"3D", "6D", "7H", "QD", "QS"}, 1)]
-    [InlineData(new[] {"2H", "2D", "4C", "4D", "4S"}, new[] {"3C", "3D", "3S", "9S", "9D"}, 1)]
-    [InlineData(new[] {"3C", "3D", "4C", "4D", "5D"}, new[] {"3H", "3S", "4H", "4S", "6D"}, -1)]
-    [InlineData(new[] {"3C", "3D", "4C", "4D", "5C"}, new[] {"2H", "2S", "4H", "4S", "6D"}, 1)]
-    [InlineData(new[] {"3C", "3D", "4C", "4D", "2C"}, new[] {"3H", "3S", "4H", "4S", "2D"}, 0)]
-    [InlineData(new[] {"3C", "3D", "3H", "3S", "2C"}, new[] {"3H", "3S", "4H", "4S", "2D"}, 1)]
-    [InlineData(new[] {"TC", "JC", "QC", "KC", "AC"}, new[] {"9H", "TH", "JH", "QH", "KH"}, 1)]
+    [InlineData(new[] { "5H", "5C", "6S", "7S", "KD" }, new[] { "2C", "3S", "8S", "8D", "TD" }, -1)]
+    [InlineData(new[] { "5D", "8C", "9S", "JS", "AC" }, new[] { "2C", "5C", "7D", "8S", "QH" }, 1)]
+    [InlineData(new[] { "2D", "9C", "AS", "AH", "AC" }, new[] { "3D", "6D", "7D", "TD", "QD" }, -1)]
+    [InlineData(new[] { "4D", "6S", "9H", "QH", "QC" }, new[] { "3D", "6D", "7H", "QD", "QS" }, 1)]
+    [InlineData(new[] { "2H", "2D", "4C", "4D", "4S" }, new[] { "3C", "3D", "3S", "9S", "9D" }, 1)]
+    [InlineData(new[] { "3C", "3D", "4C", "4D", "5D" }, new[] { "3H", "3S", "4H", "4S", "6D" }, -1)]
+    [InlineData(new[] { "3C", "3D", "4C", "4D", "5C" }, new[] { "2H", "2S", "4H", "4S", "6D" }, 1)]
+    [InlineData(new[] { "3C", "3D", "4C", "4D", "2C" }, new[] { "3H", "3S", "4H", "4S", "2D" }, 0)]
+    [InlineData(new[] { "3C", "3D", "3H", "3S", "2C" }, new[] { "3H", "3S", "4H", "4S", "2D" }, 1)]
+    [InlineData(new[] { "TC", "JC", "QC", "KC", "AC" }, new[] { "9H", "TH", "JH", "QH", "KH" }, 1)]
     public void ShouldSolveExample(string[] player1, string[] player2, int expected)
     {
         // Arrange
