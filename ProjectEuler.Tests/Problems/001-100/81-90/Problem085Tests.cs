68f396a48076b9acb8b5096649772cc1a85994a7
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-06-03 23:04:15 +0200

    Solve Problem 85

---
+++ ProjectEuler.Tests/Problems/001-100/81-90/Problem085Tests.cs
@@ -0,0 +1,42 @@
+﻿using ProjectEuler.Problems._001_100._81_90;
+
+namespace ProjectEuler.Tests.Problems._001_100._81_90;
+
+public class Problem085Tests : ProblemTestBase
+{
+    protected override IProblem Problem => new Problem085();
+
+    protected override string Answer => "2772";
+
+    [Fact]
+    public async Task Should_SolveExample()
+    {
+        // Arrange
+        var args = new[] { "18" };
+
+        // Act
+        var result = await this.Problem.CalculateAsync(args);
+
+        // Assert
+        Assert.Equal("6", result);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 1)]
+    [InlineData(1, 2, 3)]
+    [InlineData(1, 3, 6)]
+    [InlineData(2, 2, 9)]
+    [InlineData(1, 4, 10)]
+    [InlineData(1, 5, 15)]
+    [InlineData(3, 2, 18)]
+    [InlineData(2, 4, 30)]
+    [InlineData(3, 3, 36)]
+    public void Should_GetSquares(int x, int y, int expected)
+    {
+        // Act
+        var result = Problem085.GetSquares(x, y);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ ProjectEuler/Problems/001-100/81-90/Problem085.cs
@@ -0,0 +1,51 @@
+﻿
+namespace ProjectEuler.Problems._001_100._81_90;
+
+public class Problem085 : IProblem
+{
+    public Task<string> CalculateAsync(string[] args)
+    {
+        if (!int.TryParse(args.FirstOrDefault(), out var target))
+        {
+            target = 2_000_000; // default value
+        }
+
+        var closestArea = 0;
+        var closest = int.MaxValue;
+        for (int i = 1; i < 1000; i++)
+        {
+            for (int j = 1; j < 1000; j++)
+            {
+                var squares = GetSquares(i, j);
+                if (squares > target)
+                {
+                    break;
+                }
+
+                if (Math.Abs(target - squares) < closest)
+                {
+                    closestArea = i * j;
+                    closest = Math.Abs(target - squares);
+                }
+            }
+        }
+
+        return Task.FromResult(closestArea.ToString());
+    }
+
+    public static int GetSquares(int x, int y)
+    {
+        var sum = 0;
+        for (int i = 1; i <= x; i++)
+        {
+            for (int j = 1; j <= y; j++)
+            {
+                var a = x - i + 1;
+                var b = y - j + 1;
+                sum += a * b;
+            }
+        }
+
+        return sum;
+    }
+}
