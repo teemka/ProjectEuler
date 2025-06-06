8791530f1011b76c7729a152f01c38682160bcdf
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-06-06 23:13:33 +0200

    Fix issue with possible numbers

---
+++ ProjectEuler.Tests/Problems/001-100/91-100/Problem096Tests.cs
@@ -0,0 +1,17 @@
+﻿
+using ProjectEuler.Problems._001_100._91_100;
+
+namespace ProjectEuler.Tests.Problems._001_100._91_100;
+
+public class Problem096Tests : ProblemTestBase
+{
+    protected override IProblem Problem => new Problem096();
+
+    protected override string Answer => "";
+
+    [Fact]
+    public void Should_SolveExample()
+    {
+
+    }
+}
