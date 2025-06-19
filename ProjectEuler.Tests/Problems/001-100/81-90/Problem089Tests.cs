ab5416d1857538c38073af27723f130e3476a50e
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-06-19 10:50:11 +0200

    Solve Problem 89

---
+++ ProjectEuler.Tests/Problems/001-100/81-90/Problem089Tests.cs
@@ -0,0 +1,10 @@
+﻿using ProjectEuler.Problems._001_100._81_90;
+
+namespace ProjectEuler.Tests.Problems._001_100._81_90;
+
+public class Problem089Tests : ProblemTestBase
+{
+    protected override IProblem Problem => new Problem089();
+
+    protected override string Answer => "743";
+}
