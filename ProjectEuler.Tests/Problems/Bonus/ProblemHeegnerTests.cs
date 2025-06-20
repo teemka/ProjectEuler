60b95e7d8d75ef552a97f29bfbf827a78cb93895
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-06-20 21:51:03 +0200

    Try solve Problem Heegner

---
+++ ProjectEuler.Tests/Problems/Bonus/ProblemHeegnerTests.cs
@@ -0,0 +1,9 @@
+﻿using ProjectEuler.Problems.Bonus;
+
+namespace ProjectEuler.Tests.Problems.Bonus;
+
+public class ProblemHeegnerTests : ProblemTestBase
+{
+    protected override IProblem Problem => new ProblemHeegner();
+    protected override string Answer => "";
+}
---
+++ ProjectEuler/Problems/Bonus/ProblemHeegner.cs
@@ -0,0 +1,35 @@
+﻿namespace ProjectEuler.Problems.Bonus;
+
+/// <summary>
+/// https://projecteuler.net/problem=heegner
+/// </summary>
+public class ProblemHeegner : IProblem
+{
+    public Task<string> CalculateAsync(string[] args)
+    {
+        var min = int.MaxValue;
+        var minDiff = double.MaxValue;
+        for (int n = -100; n <= 1000; n++)
+        {
+            if (n >= 0 && double.IsInteger(Math.Sqrt(n)))
+            {
+                continue;
+            }
+
+            // cos ix = cosh x
+            var value = n < 0
+                ? Math.Cosh(Math.PI * Math.Sqrt(-n))
+                : Math.Cos(Math.PI * Math.Sqrt(n));
+
+            var diff = Math.Abs(value - Math.Round(value));
+
+            if (diff < minDiff)
+            {
+                minDiff = diff;
+                min = n;
+            }
+        }
+
+        return Task.FromResult(min.ToString());
+    }
+}
