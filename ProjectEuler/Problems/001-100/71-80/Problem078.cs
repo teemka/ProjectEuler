d9a04a40cad426102f4a31c946859148cb70054e
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-06-22 23:32:42 +0200

    Try Brute forcing

--- ProjectEuler/Problems/001-100/71-80/Problem078.cs
+++ ProjectEuler/Problems/001-100/71-80/Problem078.cs
@@ -1,22 +1,51 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 
 namespace ProjectEuler.Problems._001_100._71_80;
 
 public class Problem078(ILogger<Problem078> logger) : IProblem
 {
+    private readonly Lock @lock = new();
+    private readonly ConcurrentDictionary<int, int> summations = [];
+
     public Task<string> CalculateAsync(string[] args)
     {
-        const int million = 1_000_000;
-        var n = 2;
-        List<int> arr = [1];
-        while (true)
+        if (File.Exists("memoization.txt"))
         {
-            if (n % 100 == 0)
+            var lines = File.ReadAllLines("memoization.txt");
+            foreach (var line in lines)
             {
-                logger.LogInformation("N: {N}", n);
+                var split = line.Split(':');
+                var n = int.Parse(split[0]);
+                var p = int.Parse(split[1]);
+                this.summations.TryAdd(n, p);
             }
 
-            var table = new long[n + 1];
+            File.WriteAllLines("memoization.txt", this.summations.OrderBy(x => x.Key).Select(x => x.Key + ":" + x.Value));
+        }
+
+        using var sw = File.AppendText("memoization.txt");
+
+        const int range = 30_000;
+        const int million = 1_000_000;
+        var output = 0;
+        var arr = Enumerable.Range(1, range).ToList();
+
+        Parallel.For(2, range, (n, state) =>
+        {
+            if (this.summations.TryGetValue(n, out var p))
+            {
+                if (p % million == 0)
+                {
+                    output = n;
+                    state.Break();
+                }
+                return;
+            }
+
+            logger.LogInformation("N: {N}", n);
+
+            var table = new int[n + 1];
             table[0] = 1;
             for (var i = 0; i < n - 1; i++)
             {
@@ -26,24 +55,28 @@ public class Problem078(ILogger<Problem078> logger) : IProblem
                     {
                         var a = arr[i];
                         var b = j - a;
-                        var c = table[b];
+                        var c = table[b] % million;
                         table[j] %= million;
                         table[j] += c;
                     }
                 }
             }
 
-            var p = table[n] + 1;
+            p = table[n] + 1;
+            lock (@lock)
+            {
+                sw.WriteLine($"{n}:{p}");
+                sw.Flush();
+            }
+            this.summations.TryAdd(n, p);
 
             if (p % million == 0)
             {
-                break;
+                output = n;
+                state.Break();
             }
+        });
 
-            arr.Add(arr[^1] + 1);
-            n++;
-        }
-
-        return Task.FromResult(n.ToString());
+        return Task.FromResult(output.ToString());
     }
 }
