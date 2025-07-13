a7bb84d9a3d594c6c7acc28823a384937d677e63
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-07-13 12:16:24 +0200

    Refactor Problem 78 after solving

--- ProjectEuler.Tests/Problems/001-100/71-80/Problem078Tests.cs
+++ ProjectEuler.Tests/Problems/001-100/71-80/Problem078Tests.cs
@@ -1,11 +1,10 @@
-using Microsoft.Extensions.Logging.Abstractions;
 using ProjectEuler.Problems._001_100._71_80;
 
 namespace ProjectEuler.Tests.Problems._001_100._71_80;
 
 public class Problem078Tests : ProblemTestBase
 {
-    protected override IProblem Problem => new Problem078(NullLogger<Problem078>.Instance);
+    protected override IProblem Problem => new Problem078();
 
-    protected override string Answer => "";
+    protected override string Answer => "55374";
 }
