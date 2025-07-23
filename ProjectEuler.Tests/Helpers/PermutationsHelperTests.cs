ce7669e90f816ec9335d55e5bd72018d13a4835b
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-07-23 22:21:23 +0200

    Optimize GetPermutations

---
+++ ProjectEuler.Tests/Helpers/PermutationsHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using ProjectEuler.Helpers;
+
+namespace ProjectEuler.Tests.Helpers;
+
+public class PermutationsHelperTests
+{
+    [Fact]
+    public void Should_GetPermutations()
+    {
+        // Arrange
+        int[] arr = [1, 2, 3];
+
+        // Act
+        var result = arr.GetPermutations().Select(x => x.ToList());
+
+        // Assert
+        Assert.Equal(
+            [
+                [1, 2, 3],
+                [2, 1, 3],
+                [3, 1, 2],
+                [1, 3, 2],
+                [2, 3, 1],
+                [3, 2, 1],
+            ],
+            result);
+    }
+}
