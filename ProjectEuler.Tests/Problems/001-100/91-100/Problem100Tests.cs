722b475b113bff652d2e493696e5e58aaeb70c98
Tomasz Buchholz <tomaszbuchholz@gmail.com>
2025-11-11 17:12:05 +0100

    Try solve problem 100

---
+++ ProjectEuler.Tests/Problems/001-100/91-100/Problem100Tests.cs
@@ -0,0 +1,16 @@
+﻿using ProjectEuler.Problems._001_100._91_100;
+
+namespace ProjectEuler.Tests.Problems._001_100._91_100;
+
+public class Problem100Tests : ProblemTestBase
+{
+    protected override IProblem Problem { get; } = new Problem100();
+
+    protected override string Answer { get; } = "";
+
+    [Fact]
+    public void ShouldSolveExample()
+    {
+        this.Problem.CalculateAsync([]);
+    }
+}
